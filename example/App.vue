6c33eb481394b9119f666d1998207c826806d2b0
Fran Dios <frankdiox@gmail.com>
2019-03-23 02:27:35 +0900

    Remove boilerplate CSS

--- example/App.vue
+++ example/App.vue
@@ -54,15 +54,6 @@ export default {
 </script>
 
 <style scoped>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  background-color: rgb(250, 250, 250);
-}
-
 .wrapper {
   max-width: 600px;
   width: 100%;
